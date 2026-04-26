8074c9e9b361235d1b0f251cc89c7b69c04f2f0b
Ilham V. Abbasov <ilhamva@code.local>
2026-04-26 12:54:45 +0400

    yeni

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace just_cargo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ models/Package.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace just_cargo.models
+{
+    internal class Package
+    {
+    }
+}
